c9bbe32969805649c80949e89274bf0c94e0595e
Neta Zmora <neta.zmora@intel.com>
2015-12-16 20:52:06 +0200

    Multiple goodies
    Search filter
    Multi-line description field
    Display only first line of description field
    Paging support for displaying ARs
    Choose Status field from drop-down

--- ars-server.js
+++ ars-server.js
@@ -20,8 +20,9 @@ function create_ar(req, res) {
         return;
       }
 
-      console.log('connected as id ' + connection.threadId + '+' + req.body.Summary);
-      connection.query('insert into ARs (MeetingID, Status, Summary) values (?, ?, ?)', [req.body.MeetingID, req.body.Status, req.body.Summary], function(err, result) {
+      console.log('CREATE: connected as id ' + connection.threadId + '+' + req.body.Description);
+      connection.query('insert into ARs (MeetingID, Status, Description, OpenDate, DueDate) values (?, ?, ?, ?, ?)',
+                       [req.body.MeetingID, req.body.Status, req.body.Description, req.body.OpenDate, req.body.DueDate], function(err, result) {
         if (err) throw err
       });
   });
@@ -34,15 +35,14 @@ function update_ar(req, res) {
         res.json({"code" : 100, "status" : "Error in connection database"});
         return;
       }
-      console.log('UPDATE connected as id ' + connection.threadId + ' AR=' + req.body.id);
-      connection.query('update ARs set Status=?, Summary=? where ArID=?', [req.body.Status, req.body.Summary, req.body.id], function(err, result) {
+      console.log('UPDATE connected as id ' + connection.threadId + ' AR=' + req.body.ArID);
+      connection.query('update ARs set Status=?, Description=? where ArID=?', [req.body.Status, req.body.Description, req.body.ArID], function(err, result) {
         if (err) throw err
       });
   });
 }
 
 function delete_ar(req, res) {
-
   pool.getConnection(function(err,connection){
       if (err) {
         connection.release();
@@ -65,8 +65,7 @@ function get_list(req,res) {
           return;
         }
 
-        console.log('connected as id ' + connection.threadId);
-
+        console.log('LIST: connected as id ' + connection.threadId);
         connection.query("select * from ARs",function(err,rows){
             connection.release();
             if(!err) {
@@ -85,7 +84,7 @@ function get_list(req,res) {
 app.use(bodyParser.json())
 
 // Event listeners
-app.get("/ars.json",function(req,res) { get_list(req,res); });
+app.get("/list_ars",function(req,res) { get_list(req,res); });
 app.post("/create_ar",function(req,res) { create_ar(req, res); });
 app.post("/update_ar",function(req,res) { update_ar(req, res); });
 app.post("/delete_ar",function(req,res) { delete_ar(req, res); });
--- public/ars_ctrl.js
+++ public/ars_ctrl.js
@@ -1,11 +1,13 @@
-var app = angular.module('ActionRequiredApp', [])
+var app = angular.module('ActionRequiredApp', ['smart-table']);
+
 app.controller('ArCtrl', function ($scope, $http) {
-  $http.get('ars.json').success(function(data) {
+  $http.get('list_ars').success(function(data) {
+    $scope.displayed=[];
     $scope.meetings = data;
   });
 
   $scope.initAR = function() {
-      $scope.newAR = {ArID:0,Summary:"", Status:""};
+      $scope.newAR = {ArID:0,Description:"", Status:""};
       $scope.editMode = null;
   }
 
@@ -21,10 +23,10 @@ app.controller('ArCtrl', function ($scope, $http) {
       if ($scope.editMode == 'edit') {
         $scope.editMode = null;
         var ArID = $scope.newAR.ArID;
-        var data = {ArID, Status:$scope.newAR.Status, Summary:$scope.newAR.Summary};
+        var data = {ArID, Status:$scope.newAR.Status, Description:$scope.newAR.Description};
 
         var arrayIdx = $scope.findArrayIdx(ArID);
-        $scope.meetings[arrayIdx].Summary = $scope.newAR.Summary;
+        $scope.meetings[arrayIdx].Description = $scope.newAR.Description;
         $scope.meetings[arrayIdx].Status = $scope.newAR.Status;
 
         $http.post('/update_ar', data).then(
@@ -37,7 +39,13 @@ app.controller('ArCtrl', function ($scope, $http) {
         return;
       }
 
-      var data = {MeetingID:8, Status:$scope.newAR.Status, Summary:$scope.newAR.Summary};
+      var today = new Date();
+      $scope.newAR.OpenDate = today.toISOString();
+      $scope.newAR.DueDate = today.toISOString();
+      var data = { MeetingID:8, Status:$scope.newAR.Status,
+                   OpenDate:$scope.newAR.OpenDate,
+                   DueDate:$scope.newAR.DueDate,
+                   Description:$scope.newAR.Description};
 
       $scope.meetings.push(data);
       $http.post('/create_ar', data).then(
@@ -51,24 +59,19 @@ app.controller('ArCtrl', function ($scope, $http) {
       return $scope.meetings;
   };
 
-  $scope.frame_url = "ars_list.html";
-  $scope.error = false;
-  $scope.incomplete = false;
-  $scope.initAR();
-
   $scope.editAR = function(ArID) {
     console.log("been here " + ArID);
-    $scope.$parent.frame_url = "ars_edit.html";
+    //$scope.$parent.frame_url = "ars_edit.html";
     //$scope.$parent.$apply(function () {$scope.frame_url = "ars_edit.html"; });
 
     if (ArID == 'new') {
-      $scope.newAR = {Summary:"", Status:""};
+      $scope.newAR = {Description:"", Status:"Open"};
       $scope.editMode = 'new';
     } else {
       $scope.editMode = 'edit';
       $scope.newAR.ArID = ArID;
       var arrayIdx = $scope.findArrayIdx(ArID);
-      $scope.newAR.Summary = $scope.meetings[arrayIdx].Summary;
+      $scope.newAR.Description = $scope.meetings[arrayIdx].Description;
       $scope.newAR.Status = $scope.meetings[arrayIdx].Status;
     }
   }
@@ -89,6 +92,52 @@ app.controller('ArCtrl', function ($scope, $http) {
 
   $scope.cancelEdit = function() {
     $scope.initAR();
-    $scope.frame_url = "ars_list.html";
+    //$scope.frame_url = "ars_list.html";
   }
+
+  $scope.error = false;
+  $scope.incomplete = false;
+  $scope.initAR();
+  //copy the references (you could clone ie angular.copy but then have to go through a dirty checking for the matches)
+  $scope.displayedCollection = [].concat($scope.meetings);
+});
+
+app.filter('lineFilter', function () {
+  return function(input) {
+    if(!input) return input;
+    var firstLine = input.split('\n')[0];
+    return firstLine;
+  };
+});
+
+app.directive('contenteditable', function() {
+return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+        // view -> model
+        elm.bind('blur', function() {
+            scope.$apply(function() {
+                ctrl.$setViewValue(elm.html());
+            });
+        });
+
+        // model -> view
+        ctrl.render = function(value) {
+            elm.html(value);
+        };
+
+        elm.bind('keydown', function(event) {
+            console.log("keydown " + event.which);
+            var esc = event.which == 27,
+                el = event.target;
+
+            if (esc) {
+                console.log("esc");
+                ctrl.$setViewValue(elm.html());
+                el.blur();
+                event.preventDefault();
+            }
+        });
+    }
+};
 });
